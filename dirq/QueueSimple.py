e62ad46dbf3b86b66795ee203dc3673dedd834f3
Konstantin Skaburskas <Konstantin.Skaburskas@gmail.com>
2011-11-04 15:25:34 +0100

    Added dummy QueueBase and QueueSimple modules.

---
+++ dirq/QueueSimple.py
@@ -0,0 +1,17 @@
+_add_data()
+_add_dir()
+_add_path()
+class QueueSimple(object):
+    ''
+    __init__()
+    add()
+    add_path()
+    add_ref()
+    count()
+    get()
+    get_path()
+    get_ref()
+    lock()
+    purge()
+    remove()
+    unlock()
