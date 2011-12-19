41fe378c3c314907a95ece0244aa936bb69aa5f0
Massimo <massimo.paladin@gmail.com>
2011-12-19 17:40:28 +0100

    syntax made compatible with sphinx doc system

--- dirq/Exceptions.py
+++ dirq/Exceptions.py
@@ -1,5 +1,20 @@
+"""Exceptions used in the module.
+
+Author
+------
+
+Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
+
+License and Copyright
+---------------------
+
+ASL 2.0
+
+Copyright (C) 2010-2011
+"""
+
 class QueueError(Exception):
-    ''
+    """QueueError"""
 
 class QueueLockError(QueueError):
-    ''
+    """QueueLockError"""
