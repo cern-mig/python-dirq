854d08a52053b18ab83798d9753c8dd604a90e59
Massimo <massimo.paladin@gmail.com>
2013-03-14 09:57:03 +0100

    updating python supported versions

--- setup.py
+++ setup.py
@@ -29,7 +29,11 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 2.4",
     "Programming Language :: Python :: 2.5",
     "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3.0",
+    "Programming Language :: Python :: 3.1",
+    "Programming Language :: Python :: 3.2",
+    "Programming Language :: Python :: 3.3",
     "Topic :: Software Development :: Libraries :: Python Modules"
 ]
 
