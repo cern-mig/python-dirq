7d3f650c6c098596bae7b68fb404f7897c62b252
Lionel Cons <lionel.cons@cern.ch>
2013-05-27 13:25:38 +0200

    cleanup packaging

--- sdist.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-# Bundle source release.
-
-RELEASE=${1:?"Provide tag number to bundle into source release."}
-
-git checkout -q rel-$RELEASE
-python setup.py sdist
-git checkout master
